9ba2a31ca598cccebd08f7b8d47b51c166f17217
Atom <fromatom@gmail.com>
2015-09-09 13:07:33 +0900

    ランダムに出す

--- main.rb
+++ main.rb
@@ -4,10 +4,22 @@ require 'net/http'
 require 'uri'
 
 get '/' do
-  text = 'なるほど'
-  fontsize = 16
-  background_color = '3d4070'
-  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color}/ffffff/300x100.png?text=#{text}")
+  fontsize = 24
+  message_set = [
+    'えんもたけなわ',
+    '機械を通すと俺の心が通らなくなる'
+    'えっち、すけっち、わんたっち'
+  ]
+  background_color_set = [
+    '3d4070',
+    'E6503B',
+    '3CA39C',
+    'D8505C',
+    'EE7E94',
+    '0074BC',
+    '98BE17'
+  ]
+  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color_set.sample()}/ffffff/300x100.png?text=#{message_set.sample()}")
 
   content_type 'image/png'
   return Net::HTTP.get_response(URI.parse(url)).body
