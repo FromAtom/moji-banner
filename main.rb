c201498137150eca6502c3ec80bf8752e39f8a85
Atom <fromatom@gmail.com>
2016-02-08 14:29:15 +0900

    微調整

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'uri'
 
 get '/' do
-  fontsize = 34
+  fontsize = 16
   message_set = [
     'えっち、すけっち、わんたっち',
     'スラムはいいぞ',
@@ -20,7 +20,7 @@ get '/' do
     '0074BC',
     '98BE17'
   ]
-  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color_set.sample()}/ffffff/600x100.png?text=#{message_set.sample()}")
+  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color_set.sample()}/ffffff/240x100.png?text=#{message_set.sample()}")
 
   content_type 'image/png'
   return Net::HTTP.get_response(URI.parse(url)).body
