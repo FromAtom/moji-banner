c3560acfd508731c968813be0533261b479f9c8d
Atom <fromatom@gmail.com>
2016-02-08 14:24:29 +0900

    文言を変更

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@ require 'net/http'
 require 'uri'
 
 get '/' do
-  fontsize = 24
+  fontsize = 34
   message_set = [
-    'えんもたけなわ',
-    '機械を通すと俺の心が通らなくなる',
     'えっち、すけっち、わんたっち',
-    '人生どうでもアメリカンスピリット'
+    'スラムはいいぞ',
+    'サービス開発は筋肉と気合',
+    '文脈'
   ]
   background_color_set = [
     '3d4070',
@@ -20,7 +20,7 @@ get '/' do
     '0074BC',
     '98BE17'
   ]
-  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color_set.sample()}/ffffff/300x100.png?text=#{message_set.sample()}")
+  url = URI.escape("http://placehold.jp/#{fontsize}/#{background_color_set.sample()}/ffffff/600x100.png?text=#{message_set.sample()}")
 
   content_type 'image/png'
   return Net::HTTP.get_response(URI.parse(url)).body
